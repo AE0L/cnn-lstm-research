d7ed15742d5e58ec63f5835c122f5c8a9ef1f8ba
Aeol <carlj15.cj@gmail.com>
2021-10-29 15:45:06 +0800

    extract tweet

--- tweet_tester/twitter/models.py
+++ tweet_tester/twitter/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from tqdm import tqdm
+from pathlib import Path
 import tweepy
+import pytz
+import datetime
+import pandas as pd
 
 
 class Tweepy:
@@ -20,9 +25,67 @@ class Tweepy:
         userid = username.id
         acc_auth = 'Public' if username.protected == False else 'Private'
 
-        # userDetails(screen_name, userid, accAuth)
         return {
             'user_handle': screen_name,
             'user_id': userid,
             'user_auth': acc_auth
         }
+
+    @staticmethod
+    def get_tweets(userid, since_date):
+        access_token = '1449676365893038088-pdugze1ET3LcXHZ4MhgW5KLtDNgi2k'
+        access_token_secret = 'bT4xTEVL5IrCkv1MkrorKjzdID4RCPNVankEMmOlALNJL'
+        consumer_key = 'fSqFCU8DvkTd8XitEaWNA3ASN'
+        consumer_secret = 'fnSlukoXfY4JZtodkqpPZLL8uBg3stk7B9rzQch2tpbgP7ldkT'
+
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+
+        api = tweepy.API(auth)
+        tweets = []
+        utc = pytz.UTC
+        count = 1
+
+        date = datetime.datetime.strptime(str(since_date), "%Y-%m-%d")
+        start_date = datetime.datetime(
+            date.year, date.month, date.day, 0, 0, 0, tzinfo=utc)
+
+        print(userid)
+
+        progress = tqdm(
+            tweepy.Cursor(api.user_timeline, user_id=userid, include_rts=False, exclude_replies=True, count=100, tweet_mode='extended').items(), ascii=True, unit='tweets'
+        )
+
+        for tweet in progress:
+            progress.set_description("Processing %i tweets" % count)
+            if tweet.created_at >= start_date:
+                try:
+                    data = [tweet.created_at, tweet.full_text,
+                            tweet.user._json['screen_name'], tweet.user._json['name']]
+                    data = tuple(data)
+
+                    tweets.append(data)
+
+                    count += 1
+
+                except tweepy.TweepError as e:
+                    print(e.reason)
+                    continue
+
+                except StopIteration:
+                    break
+            else:
+                break
+
+        print("Finished. Retrieved " + str(count-1) + " tweets.")
+        df = pd.DataFrame(tweets,
+                          columns=['created_at', 'tweet_text', 'screen_name', 'name'])
+
+        # rawtweet_df = pd.DataFrame(raw_tweets,
+        # columns=['tweet_text'])
+
+        output_file = 'trial.csv'
+        output_dir = Path('output/')
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+        df.to_csv(output_dir / output_file, index=False)
