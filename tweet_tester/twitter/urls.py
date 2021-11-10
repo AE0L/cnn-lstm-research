5308b179ae1a3a2275c165248a7fcd67b4edc2f3
Meryll Daeme Cornita <76724800+meryll9@users.noreply.github.com>
2021-11-10 13:25:48 +0800

    Update urls.py

--- tweet_tester/twitter/urls.py
+++ tweet_tester/twitter/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('date/', views.date_settings, name="date_settings"),
     path('user_details/', views.search_user, name="search_user"),
     path('user_tweets/', views.extract_tweets, name="extract_tweets"),
-    path('analyze/', views.analyze_tweets, name="analyze_tweets")
+    path('analyze/', views.analyze_tweets, name="analyze_tweets"),
+    path('classify/', views.classify_user, name="classify_user")
 ]
